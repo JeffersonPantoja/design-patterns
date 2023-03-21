fdf7cc916521d2bde719b4718f40e4e97550a81f
Jefferson Pantoja <jefferson.pantoja.silva@bluesoft.com.br>
2023-03-20 22:48:33 -0300

    chain of responsibility
    Padrão que executa todos os handler

---
+++ design-patterns/src/main/java/patterns/behavior/chainOfResponsibility/todos/Demo.java
@@ -0,0 +1,37 @@
+package patterns.behavior.chainOfResponsibility.todos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Demo {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static Server server;
+
+    private static void init() {
+        server = new Server();
+        server.register("admin@example.com", "adm123");
+        server.register("jeff@example.com", "jeff123");
+
+        Middleware middleware = Middleware.link(
+            new ThrottlingMiddleware(2),
+            new UserExistsMiddleware(server),
+            new RoleCheckMiddleware()
+        );
+
+        server.setMiddleware(middleware);
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        boolean success;
+        do {
+            System.out.print("Enter email: ");
+            String email = reader.readLine();
+            System.out.print("Input password: ");
+            String password = reader.readLine();
+            success = server.logIn(email, password);
+        } while (!success);
+    }
+}
